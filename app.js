e73204244259e656b0bf6e0a280a1c40f539a674
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-02-21 12:04:18 -0300

    Added findById books route and functionallity.

--- app.js
+++ app.js
@@ -31,6 +31,18 @@ bookRouter.route('/books')
 
   });
 
+bookRouter.route('/books/:bookId')
+  .get(function(req, res) {
+    var bookId = req.params.bookId; // example: 56c9d11d3b6c8e7e9977b55d
+    Book.findById(bookId, function (err, book) {
+      if(err) {
+        res.status(500).send(err);
+      } else {
+        res.json(book);
+      }
+    });
+  });
+
 app.use('/api', bookRouter);
 
 app.get('/', function(req, res) {
