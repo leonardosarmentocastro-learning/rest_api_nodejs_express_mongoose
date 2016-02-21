3b1c3d5459e7ec63572ae01c4503ca16a344f6b3
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-02-21 11:51:58 -0300

    Added Book model, http get for all boks and functionallity of filtering those books by Genre.

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-var express = require('express');
+var express = require('express'),
+mongoose = require('mongoose');
+
+
+var db = mongoose.connect("mongodb://localhost/bookAPI");
+var Book = require('./models/bookModel');
+
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+
+
+
 var bookRouter = express.Router();
 bookRouter.route('/books')
   .get(function(req, res) {
-    var responseJson = {
-      hello: "This is my api :)"
+
+    var query = {};
+    if(req.query.genre) {
+      query.genre = req.query.genre;
     }
 
-    res.json(responseJson);
+    Book.find(query, function(err, books) {
+      if(err) {
+        res.status(500).send(err);
+      } else {
+        res.json(books);
+      }
+    });
+
   });
 
 app.use('/api', bookRouter);
