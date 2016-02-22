8834b8425f23b6d21dca9522f66fa6a7a0792d05
Leonardo Sarmento de Castro <leonardo.sarmentocastro@gmail.com>
2016-02-21 21:13:35 -0300

    Added body-parser dependency; Created '/books' post route.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-mongoose = require('mongoose');
+mongoose = require('mongoose'),
+bodyParser = require('body-parser');
 
 
 var db = mongoose.connect("mongodb://localhost/bookAPI");
@@ -9,11 +10,18 @@ var Book = require('./models/bookModel');
 var app = express();
 var port = process.env.PORT || 3000;
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 var bookRouter = express.Router();
 bookRouter.route('/books')
+  .post(function (req, res) {
+    var book = new Book(req.body);
+
+    book.save();
+    res.status(201).send(book);
+  })
   .get(function(req, res) {
 
     var query = {};
